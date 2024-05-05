256e096624fd0ddd5eec0cc8ad006aa8a854996f
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-05-05 22:45:37 +0300

    step 36

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const addOrUpdateTask = () => {
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value,
+    };
+    if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+    }
+}
+
 const reset = () => {
     titleInput.value = "";
     dateInput.value = "";
@@ -27,10 +40,10 @@ openTaskFormBtn.addEventListener("click", () => {
 
 closeTaskFormBtn.addEventListener("click", () => {
     const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
-    if(formInputsContainValues) {
+    if (formInputsContainValues) {
         confirmCloseDialog.showModal();
     } else {
-    reset();
+        reset();
     }
 });
 
@@ -45,17 +58,6 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", e => {
     e.preventDefault();
-
-    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-    const taskObj = {
-        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-        title: titleInput.value,
-        date: dateInput.value,
-        description: descriptionInput.value,
-    };
-    if (dataArrIndex === -1) {
-        taskData.unshift(taskObj);
-    }
 
     taskData.forEach(({id, title, date, description}) => {
             (tasksContainer.innerHTML += `
