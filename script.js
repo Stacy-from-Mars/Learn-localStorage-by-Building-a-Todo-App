6d7755d5c8756206919b3cc40cf5434b05f9042d
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-05-10 23:51:07 +0300

    step 49

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const editTask = (buttonEl) => {
     titleInput.value = currentTask.title;
     dateInput.value = currentTask.date;
     descriptionInput.value = currentTask.description;
-
+    addOrUpdateTaskBtn.innerText = "Update Task";
 }
 
 const reset = () => {
